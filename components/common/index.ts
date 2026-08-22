eff2a5eede030fc7ad66a46d54bd2e916b5fdf4c
Yogank sharma <yogank2005@gmail.com>
2026-08-22 15:57:28 +0530

    Delete components directory

--- components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Layout } from './Layout';
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Loading } from './Loading';
-export { default as SEO } from './SEO';
