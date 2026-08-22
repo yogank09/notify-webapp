ebd77fd51fd64702a8a9082c9083d08447ccc620
Yogank sharma <yogank2005@gmail.com>
2026-08-23 00:47:41 +0530

    Delete utils directory

--- utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { formatDate, formatDateTime } from './formatDate';
-export { truncateText } from './truncateText';
-export { fetchFallbackMetalPrice } from './goldPriceFallback';
--- utils/truncateText.ts
+++
@@ -1,4 +0,0 @@
-export const truncateText = (text: string, maxLength: number = 200): string => {
-  if (!text) return '';
-  return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
-};
