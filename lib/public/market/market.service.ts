ea11673262f0e313878a2dccbb068d2f52065f80
Yogank sharma <yogank2005@gmail.com>
2026-08-23 00:48:11 +0530

    Delete lib/public directory

--- lib/public/market/market.service.ts
+++
@@ -1,9 +0,0 @@
-import api from '../../../services/api';
-import { IMarketAllResponse } from '../../../types';
-
-export const marketService = {
-  getAll: async (): Promise<IMarketAllResponse> => {
-    const { data } = await api.get('/market/all');
-    return data;
-  },
-};
--- lib/public/metalPrices/metalPrices.service.ts
+++
@@ -1,9 +0,0 @@
-import api from '../../../services/api';
-import { IMetalPriceResponse } from '../../../types';
-
-export const metalPricesService = {
-  getAll: async (params?: { page?: number; limit?: number }): Promise<IMetalPriceResponse> => {
-    const { data } = await api.get('/metal-prices', { params });
-    return data;
-  },
-};
