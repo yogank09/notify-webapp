816d92f10501c375a35b7c25cd0d1c4fa28e7356
Yogank sharma <yogank2005@gmail.com>
2026-08-22 15:57:48 +0530

    Delete hooks directory

--- hooks/useAppSelector.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
-import type { RootState } from '@store/index';
-
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export default useAppSelector;
